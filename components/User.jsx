8100c9e26fb5155206d438ddd00bbf02b7c5f934
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2023-01-05 01:18:44 +0100

    user authorization flow

--- components/User.jsx
+++ components/User.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
 
-const User = props => {
+const User = () => {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <Image
+        className='w-10 h-10 rounded-full p-1 hover:bg-gray-200 cursor-pointer'
+        src={session.user.image}
+        onClick={signOut}
+        width={32}
+        height={32}
+        alt="user-image"
+      />
+    )
+  }
   return (
-    <div>User</div>
+    <button className='bg-blue-500 text-white px-6 py-2  font-medium rounded-md hover:brightness-105 hover:shadow-md' onClick={signIn}>Sign in</button>
   )
 }
-
-User.propTypes = {}
 
 export default User
