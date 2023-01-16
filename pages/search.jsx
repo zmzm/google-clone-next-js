4968a228e9e61961fe15674ed579e07e627616f7
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2023-01-16 20:02:38 +0100

    get data from google api

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import SearchHeader from '../components/SearchHeader';
+import dataMock from '../mocks/dataMock.json';
 
-const Search = () => {
+const Search = ({ data }) => {
+  console.log(data);
   return (
     <div>
       <Head>
@@ -12,5 +14,15 @@ const Search = () => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  // const { query: { q, searchType } = {} } = context;
+  /* const data = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.GOOGLE_SEARCH_CX_KEY}&q=${q}&searchType=${searchType}`
+  ).then((response) => response.json()); */
+  return {
+    props: { data: dataMock },
+  };
+}
 
 export default Search;
